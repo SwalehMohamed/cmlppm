8055ff3b4a92ed13e7cac329321f6de85eb86451
SwalehMohamed <elbusaidyswaleh052@gmail.com>
2024-05-16 12:09:32 +0300

    complete course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Mombasa Canvas Limited PM',
+    default: 'Mombasa Canvas Limited PM',
+  },
+  description: 'The official sit to check progress of Production',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
